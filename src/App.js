ef49594c2370d32199020cf2241bdd1e2f02070f
manish198 <gautam.manish.manish@gmail.com>
2023-03-23 13:23:15 -0400

    Add First Component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Rental Management System!!!</h1>
+      <Button variant='contained'>Add a Property</Button>
+    </div>
+  );
+}
+
+export default App;
